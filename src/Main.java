1540c219e60891d01aae8b2fdb5cf5ea4d3bd1bd
aibalit200 <135988332+aibalit200@users.noreply.github.com>
2024-12-13 23:46:05 +0500

    Added a welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to version control with Git!");
+        System.out.println(" Welcome to version control with Git! almat");
     }
 }
